7ddc76e6d94f0f6a3cf3bf92413100c47c2a0617
PonnyHydro <lildavidka@gmail.com>
2022-10-18 20:39:45 +0200

    deploy fix

--- API/index.js
+++ API/index.js
@@ -10,7 +10,7 @@ export const SendEmail = async ({
   try {
     const datas = { fullName, email, phone, message };
     let res = await axios.post(
-      `https:/nextjs-landing-server.vercel.app:5000/send`,
+      `https:/nextjs-landing-server.vercel.app:443/send`,
       datas
     );
     if (res) {
